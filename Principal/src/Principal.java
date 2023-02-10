c8c57acfecdfecbd933b572372508b73b43df6a9
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:37:07 -0500

    primer act

---
+++ Principal/src/Principal.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+//nombre de la clase
+public class Principal {
+
+	// metodo principal
+	public static void main(String[] args) {
+
+		System.out.println(contarPalabras());
+	}
+
+	public static String contarPalabras() {
+		System.out.println("ingrese la palabra");
+		// clase que permite capturar datos por consola
+		Scanner sc = new Scanner(System.in);
+
+		String prueba = sc.next();
+		return "la cantidad de letras que tiene la palabra es : ".concat(prueba.toUpperCase());
+
+
+	}
+
+}
